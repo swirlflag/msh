22a1e148e0daa3bac6dd2ebaa3306655faf00e09
joo <wldnsmsmmm@naver.com>
2019-03-23 15:23:10 +0900

    페이지방식에서 모듈방식으로 카피

--- src/App.js
+++ src/App.js
@@ -1,27 +1,88 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
+const Header = ({title,totalPlayers}) =>(
+  <header>
+    <h1>{ title }</h1>
+    <span className="stats">
+        Players : { totalPlayers }
+      </span>
+  </header>
+);
+
+class Counter extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name : 'a',
+      score : 30,
+    }
+    // this.incrementScore = this.incrementScore.bind(this)
+  }
+
+  incrementScore = () => {
+    this.setState(prevState => {
+      return {
+        score : prevState.score + 1
+      }
+    });
+  }
+
+  render(){
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score">{this.state.score}</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
+  }
+}
+
+const Player = (props) => (
+  <div className="player">
+    <span className="player-name">
+      <button className="remove-player" onClick={()=>{props.handleRemovePlayer(props.id)}}>X</button>
+    </span>
+    <span className="player-name">{ props.name }</span>
+    <Counter></Counter>
+  </div>
+);
+
+class App extends React.Component {
+  state = {
+    players : [
+      {id : 1, name : "MSH",},
+      {id : 2,name : "A",},
+      {id : 3,name : "B",},
+      {id : 4,name : "C",},
+      {id : 5, name : "D",},
+    ]
+  }
+
+  handleRemovePlayer = (id) => {
+    this.setState(prevState => ({
+      players: prevState.players.filter(item=> item.id != id)
+    }));
+  }
+
+  render(){
+    return (
+      <div className="scoreboard">
+        <Header title="my scoreboard" totalPlayers={ 10 + 1 }/>
+        {
+          this.state.players.map(player => (
+            <Player
+              name={player.name}
+              key={player.id.toString()}
+              handleRemovePlayer={this.handleRemovePlayer}
+              id={player.id}
+            />
+          ))
+        }
+      </div>
+    ) ;
   }
 }
 
